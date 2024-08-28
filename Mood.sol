9820b83764054562deafad996d600718e97d7d5e
Samuel Ajayi <samuelajayi554@gmail.com>
2024-08-28 07:16:52 +0100

    first commit

---
+++ Mood.sol
@@ -0,0 +1,31 @@
+0x6F95aB8C5D499A1D009E4FD55304855DcA63139f
+
+
+[
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mood",
+				"type": "string"
+			}
+		],
+		"name": "setMood",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMood",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
